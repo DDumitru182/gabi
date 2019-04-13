147faee21cb7892f0686f417d4d1c95b328d3f9b
Gabriel Chelariu <gabrielchelariu@outlook.com>
2019-04-13 14:10:11 +0300

    commit pt Conflict

---
+++ modul4-shared/src/main/java/com/sda/java8/LinkedList/Application.java
@@ -0,0 +1,10 @@
+package com.sda.java8.LinkedList;
+
+public class Application {
+    public static void main(String[] args){
+        Node myNode = new Node(20, nextElement);
+        Node myNode2 = new Node(40, nextElement);
+        Node myNode2 = new Node(50, nextElement);
+//        System.out.println(myNode);
+    }
+}
---
+++ modul4-shared/src/main/java/com/sda/java8/LinkedList/Node.java
@@ -0,0 +1,11 @@
+package com.sda.java8.LinkedList;
+
+public class Node {
+    private final int tip;
+    private final Node nextElement;
+
+    public Node(int tip/*, Node nextElement*/){
+        this.tip = tip;
+//        this.nextElement = nextElement;
+    }
+}
