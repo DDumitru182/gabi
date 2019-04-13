ed338124127b389b1fa7e408a7aee95f2a2a1505
Dan Dumitru <dan.dumitru@gmail.com>
2019-04-13 14:11:48 +0300

    djfbsdjbfsd

--- modul4-shared/src/main/java/LinkedList/LL.java
+++
@@ -1,7 +0,0 @@
-package LinkedList;
-
-public class LL {
-    public static void main(String[] args){
-        
-    }
-}
---
+++ modul4-shared/src/main/java/LinkedList/Node.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+
+public class Node {
+    private final int value;
+    private Node nextElement;
+
+
+    public Node(int value) {
+        this.value = value;
+
+    }
+}
