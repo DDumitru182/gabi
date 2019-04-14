fc5c939638567d71718bf77f050b04f504052660
Dan Dumitru <dan.dumitru@gmail.com>
2019-04-14 10:28:00 +0300

    nada

--- modul4-shared/src/main/java/com/sda/java8/LinkedList/LinkedList.java
+++ modul4-shared/src/main/java/com/sda/java8/LinkedList/LinkedList.java
@@ -4,30 +4,47 @@ public class LinkedList {
     private Node head;
 
     //Adds new node to the list
-    void add(Node node){
+    void add(Node node) {
         //Test if null then ...
         //Else: parcurge = head
         //while parcurge has a next element
         //parcurge becomes parcurge.next
         //parcurge.next = node
 
-        if (head == null){
+        if (head == null) {
             head = node;
             return;
         }
 
         Node parcurge = head;
-        while (parcurge.getNextElement() != null){
+        while (parcurge.getNextElement() != null) {
             parcurge = parcurge.getNextElement();
         }
 
         parcurge.setNextElement(node); /*LEGATURA: cea mai importanta chestie din LL*/
     }
 
+    void remove(int value) {
+
+        //exemplu remove last element if lost has 2 elements
+//        if (head.getNextElement().getNextElement() == null) {
+//            head.setNextElement(null);
+//        }
+
+//        for (Node nodCurent = head; nodCurent.getNextElement().getValue() == value; nodCurent = nodCurent.getNextElement()) {
+//can t use for bc it is not explicitludoing anythingbesides walking the list
+
+        }
+        Node nodCurent = head;
+        while (nodCurent.getNextElement() != null && (nodCurent.getNextElement().getValue() == value)) {
+            nodCurent = nodCurent.getNextElement();
+    }
+nodCurent.setNextElement(nodCurent.getNextElement().getNextElement());
+
     //Display all elements of the list separated by space
-    String print(){
+    String print() {
         StringBuilder builder = new StringBuilder();
-        for(Node nodCurent = head; nodCurent != null; nodCurent = nodCurent.getNextElement()){
+        for (Node nodCurent = head; nodCurent != null; nodCurent = nodCurent.getNextElement()) {
             builder.append(nodCurent.getTip());
             builder.append(" ");
         }
